9412268f4cdc70019f9dcf78b2104769e3928bb1
safidy1067-cmd <safidy1067@gmail.com>
2026-03-09 20:46:53 +0300

    Add files via upload

---
+++ projet.js
@@ -0,0 +1,24 @@
+    const sklillBars = document.querySelectorAll('.skill-progress');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting){
+                const width = entry.target.style.width;
+                entry.target.style.width = '0';
+                setTimeout(() => {
+                    entry.target.style.width = width;
+                },100);
+            }
+        });
+    },{threshold: 0.5});
+
+    sklillBars.forEach(bar => {
+        observer.observe(bar);
+    });
+
+    var toggle_menu = document.querySelector('.responsive-menu');
+    var menu = document.querySelector('.menu');
+    toggle_menu.onclick= function(){
+        toggle_menu.classList.toggle('active');
+        menu.classList.toggle('responsive')
+    }
